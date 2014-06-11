83c9f9342d391db18dafc61cb8c4f758b02d682d
Videl Smith <videl@fslhome.org>
2014-06-11 12:16:49 +0200

    Add base behavior for actors.

---
+++ modelisation/actor_contract.erl
@@ -0,0 +1,28 @@
+-module(actor_contract).
+
+-callback get_name() -> 
+	Name :: string().
+
+-callback get_opt() -> 
+	list(tuple(Name :: string(), Parameters :: list(atom()))).
+
+-callback start() -> 
+	ok |
+	undefined.
+
+-callback stop() -> 
+	ok |
+	undefined.
+
+-callback work_time(Time :: non_neg_integer()) ->
+	non_neg_integer() |
+	undefined.
+
+-callback get_data() ->
+	term().
+
+-callback get_previous_data(Step :: non_neg_integer()) ->
+	term().
+
+-callback get_state() ->
+	term().
