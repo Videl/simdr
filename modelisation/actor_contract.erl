6c308e051e11e290132b5318ae40c9b2d598abf8
Videl Smith <videl@fslhome.org>
2014-06-11 17:19:55 +0200

    Add create method. Need to think.

--- modelisation/actor_contract.erl
+++ modelisation/actor_contract.erl
@@ -1,7 +1,8 @@
 -module(actor_contract).
 
 -callback get_name(Config :: term()) -> 
-	Name :: string().
+	Name :: string() |
+	Name :: atom().
 
 -callback get_opt(Config :: term()) -> 
 	list(tuple(Name :: string(), Parameters :: list(atom()))) |
@@ -27,3 +28,9 @@
 
 -callback get_state(Config :: term()) ->
 	term().
+
+-callback loop(Config :: term(), ListData :: list(term())).
+
+create(Module) ->
+	Pid = spawn(Module, loop, []).
+	
