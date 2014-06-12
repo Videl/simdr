e4700965c8727eccf9832d17d46769fd92aadef0
Videl Smith <videl@fslhome.org>
2014-06-12 10:35:30 +0200

    Change of direction

--- modelisation/actor_contract.erl
+++ modelisation/actor_contract.erl
@@ -7,33 +7,7 @@
 %% Contract for Actors
 %% ===================================================================
 
--callback get_name(Config :: term()) -> 
-	Name :: string() |
-	atom().
-
--callback get_opt(Config :: term()) -> 
-	list(tuple(Name :: string(), Parameters :: list(atom()))) |
-	undefined.
-
--callback start(Config :: term()) -> 
-	ok |
-	undefined.
-
--callback stop(Config :: term()) -> 
-	ok |
-	undefined.
-
--callback work_time(Config :: term()) ->
-	non_neg_integer() |
-	undefined.
-
--callback get_data(Config :: term()) ->
-	term().
-
--callback get_previous_data(Config :: term(), Step :: non_neg_integer()) ->
-	term().
-
--callback get_state(Config :: term()) ->
+-callback answer(term()) ->
 	term().
 
 %% ===================================================================
@@ -41,7 +15,8 @@
 %% ===================================================================
 
 create(Module, Name, Work_time) ->
-	Actor = #config{module=Module, name=Name, opt=undefined, state=off, work_time=Work_time}.
+	Actor = #config{module=Module, name=Name, opt=undefined, state=off, work_time=Work_time, list_data=[]},
+	{ok, Actor}.
 
 create(Module, Name, Opt, State, Work_time, List_data ) ->
 	Actor = #config { module=Module, name=Name, opt=Opt, state=State, work_time=Work_time, list_data=List_data },
