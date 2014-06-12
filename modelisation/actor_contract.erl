2e6ebb40c14fdc07f70f1a1a7b7753cef23ad222
MarionLy <marion.ly@telecomnancy.net>
2014-06-12 11:01:22 +0200

    interface finale?

--- modelisation/actor_contract.erl
+++ modelisation/actor_contract.erl
@@ -22,12 +22,39 @@ create(Module, Name, Opt, State, Work_time, List_data ) ->
 	Actor = #config { module=Module, name=Name, opt=Opt, state=State, work_time=Work_time, list_data=List_data },
 	{ok,Actor}.
 	
-get_data(Config) ->
-	get_head_data(Config#config.list_data).
+get_data(Actor) ->
+	get_head_data(Actor#config.list_data).
 
-get_previous_data(Config, N) ->
+get_previous_data(Actor, N) ->
 	not_implemented.
 
+get_name(Actor) ->
+	Actor#config.name.
+
+get_opt(Actor) ->
+	Actor#config.opt.
+
+get_work_time(Actor) ->
+	Actor#config.work_time.
+
+get_state(Actor) ->
+	Actor#config.state.
+
+get_head_data() ->
+	Actor#config.list_data.
+
+set_work_time(Actor, Wtime:: non_neg_integer())->
+	Actor#config { wtime =Wtime},
+ 	{ok, time}.
+
+start(Actor) -> 
+	Actor#config {state = on},
+	on.
+
+stop(Actor) -> 
+	Actor#config { state = off},
+	off. 
+	
 %% ===================================================================
 %% Internal API
 %% ===================================================================
