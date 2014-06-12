43c40cb45e8caf1ccd1cdc1f19db3854fbb82f08
MarionLy <marion.ly@telecomnancy.net>
2014-06-12 11:12:15 +0200

    Exports interface ajoutés

--- modelisation/actor_contract.erl
+++ modelisation/actor_contract.erl
@@ -1,7 +1,7 @@
 -module(actor_contract).
 -include("config.hrl").
 
--export([create/6, create/3, get_data/1, get_previous_data/2]).
+-export([create/6, create/3, get_data/1, get_previous_data/2, get_id/1, get_opt/1, get_work_time/1, get_state/1, set_work_time/1, start/1, stop/1]).
 
 %% ===================================================================
 %% Contract for Actors
@@ -10,16 +10,17 @@
 -callback answer(Config :: term(), Entering :: term()) ->
 	Exiting :: term().
 
+
 %% ===================================================================
 %% Helper functions
 %% ===================================================================
 
-create(Module, Name, Work_time) ->
-	Actor = #config{module=Module, name=Name, opt=undefined, state=off, work_time=Work_time, list_data=[]},
+create(Module, Id, Work_time) ->
+	Actor = #config{module=Module, id=Id opt=undefined, state=off, work_time=Work_time, list_data=[]},
 	{ok, Actor}.
 
-create(Module, Name, Opt, State, Work_time, List_data ) ->
-	Actor = #config { module=Module, name=Name, opt=Opt, state=State, work_time=Work_time, list_data=List_data },
+create(Module, Id, Opt, State, Work_time, List_data ) ->
+	Actor = #config { module=Module,, id=Id, opt=Opt, state=State, work_time=Work_time, list_data=List_data },
 	{ok,Actor}.
 	
 get_data(Actor) ->
@@ -28,8 +29,8 @@ get_data(Actor) ->
 get_previous_data(Actor, N) ->
 	not_implemented.
 
-get_name(Actor) ->
-	Actor#config.name.
+get_id(Actor) ->
+	Actor#config.id.
 
 get_opt(Actor) ->
 	Actor#config.opt.
@@ -39,9 +40,6 @@ get_work_time(Actor) ->
 
 get_state(Actor) ->
 	Actor#config.state.
-
-get_head_data() ->
-	Actor#config.list_data.
 
 set_work_time(Actor, Wtime:: non_neg_integer())->
 	Actor#config { wtime =Wtime},
