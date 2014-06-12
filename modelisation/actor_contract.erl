5d8465ff1cb3b8268eb732babd2b20868b0c3cf3
Videl Smith <videl@fslhome.org>
2014-06-12 11:57:41 +0200

    Adds: get_previous_data + unit tests

--- modelisation/actor_contract.erl
+++ modelisation/actor_contract.erl
@@ -1,4 +1,7 @@
 -module(actor_contract).
+-include_lib("eunit/include/eunit.hrl").
+
+% config record
 -include("config.hrl").
 
 -export([create/6, create/3, get_data/1, get_previous_data/2]).
@@ -26,7 +29,7 @@ get_data(Config) ->
 	get_head_data(Config#config.list_data).
 
 get_previous_data(Config, N) ->
-	not_implemented.
+	get_previous_data_helper(Config#config.list_data, N).
 
 %% ===================================================================
 %% Internal API
@@ -37,3 +40,43 @@ get_head_data([]) ->
 
 get_head_data([Head|_Tail]) ->
 	Head.
+
+get_previous_data_helper(_List, N) when N < 0 ->
+	out_of_range;
+get_previous_data_helper([Head|_Tail], 1) ->
+	Head;
+get_previous_data_helper([], 1) ->
+	out_of_range;
+get_previous_data_helper([_Head|Tail], N) ->
+	get_previous_data_helper(Tail, N-1).
+
+%% ===================================================================
+%% Tests
+%% ===================================================================
+
+get_head_1_test() ->
+	3 = get_head_data([3,2,1]).
+
+get_head_2_test() ->
+	undefined = get_head_data([]).
+
+get_head_3_test() ->
+	1 = get_head_data([1]).
+
+get_previous_data_1_test() ->
+	2 = get_previous_data_helper([1,2,3], 2).
+
+get_previous_data_2_test() ->
+	1 = get_previous_data_helper([1,2,3], 1).
+
+get_previous_data_3_test() ->
+	3 = get_previous_data_helper([1,2,3], 3).
+
+get_previous_data_4_test() ->
+	out_of_range = get_previous_data_helper([1,2,3], 4).
+
+get_previous_data_5_test() ->
+	out_of_range = get_previous_data_helper([1,2,3], -3).
+
+get_previous_data_6_test() ->
+	4 = get_previous_data_helper([1,2,3, 4], 4).
