595c88a2afe02c5ba1d563c46cf7d5e287f2d18e
Videl Smith <videl@fslhome.org>
2014-06-12 10:27:27 +0200

    Add: some base functions

--- modelisation/actor_contract.erl
+++ modelisation/actor_contract.erl
@@ -1,7 +1,11 @@
 -module(actor_contract).
 -include("config.hrl").
 
--export([create/6]).
+-export([create/6, create/3, get_data/1, get_previous_data/2]).
+
+%% ===================================================================
+%% Contract for Actors
+%% ===================================================================
 
 -callback get_name(Config :: term()) -> 
 	Name :: string() |
@@ -32,7 +36,29 @@
 -callback get_state(Config :: term()) ->
 	term().
 
+%% ===================================================================
+%% Helper functions
+%% ===================================================================
+
+create(Module, Name, Work_time) ->
+	Actor = #config{module=Module, name=Name, opt=undefined, state=off, work_time=Work_time}.
+
 create(Module, Name, Opt, State, Work_time, List_data ) ->
 	Actor = #config { module=Module, name=Name, opt=Opt, state=State, work_time=Work_time, list_data=List_data },
 	{ok,Actor}.
 	
+get_data(Config) ->
+	get_head_data(Config#config.list_data).
+
+get_previous_data(Config, N) ->
+	not_implemented.
+
+%% ===================================================================
+%% Internal API
+%% ===================================================================
+
+get_head_data([]) ->
+	undefined;
+
+get_head_data([Head|_Tail]) ->
+	Head.
