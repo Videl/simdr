e318933a94ea49d28f65bb5c7b35b9e6b153153e
MarionLy <marion.ly@telecomnancy.net>
2014-06-13 10:59:29 +0200

    ajout test railway

--- modelisation/actor_railway.erl
+++ modelisation/actor_railway.erl
@@ -1,5 +1,6 @@
 -module(actor_railway).
 -behaviour(actor_contract).
+-include_lib("eunit/include/eunit.hrl").
 -include("config.hrl").
 
 %% Actor Contract Behaviors Callbacks
@@ -12,9 +13,18 @@
 
 -export([create/0]).
 create() ->
-   actor_contract:create(?MODULE, actor_railway, off, undefined, 10, []).
+   actor_contract:create(?MODULE, actor_railway, undefined, off, 2, []).
 
 answer(RailwayConfig, {actor_product, ProductConfig}) ->
 	actor_contract:work(actor_contract:get_work_time(RailwayConfig)),
 	{answer, actor_contract:get_state(RailwayConfig), actor_contract:get_id(ProductConfig)}.
 
+
+%% ===================================================================
+%% Tests
+%% ===================================================================
+
+answer_test() ->
+	{ok, Rail} = create(),
+	{ok, Prod} = actor_product:create(),
+	{ answer, off, 450} = answer(Rail, {actor_product, Prod}).
