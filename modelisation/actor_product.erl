c9a63be541c02c43e3e97ac910d8cea0cb4df7e4
Videl Smith <videl@fslhome.org>
2014-06-13 10:43:56 +0200

    Add actor_rfid tests.

--- modelisation/actor_product.erl
+++ modelisation/actor_product.erl
@@ -5,18 +5,28 @@
 %% Actor Contract Behaviors Callbacks
 
 -export([
-	answer/2
+	answer/2,
+	create/0
 	]).
 
 %% External API
 
--export([create/0]).
+-export([
+	create/1
+	]).
+
+%% Behavior implementation
 
 create() ->
-   actor_contract:create(?MODULE, actor_product, off, undefined, 10, []).
+   actor_contract:create(?MODULE, random_id(), undefined, raw, 0, []).
+
+create(Id) ->
+	actor_contract:create(?MODULE, Id, undefined, raw, 0, []).
 
 answer(ProductConfig, _) ->
 		{answer, actor_contract:get_state(ProductConfig), actor_contract:get_data(ProductConfig)}.
 
+%% Internal API
 
-
+random_id() ->
+	random:uniform(1000).
