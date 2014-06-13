dab54909959944ad76d0f06f407331a304149569
MarionLy <marion.ly@telecomnancy.net>
2014-06-13 10:59:18 +0200

    ajout test product

--- modelisation/actor_product.erl
+++ modelisation/actor_product.erl
@@ -1,6 +1,7 @@
 -module(actor_product).
 -behaviour(actor_contract).
 -include("config.hrl").
+-include_lib("eunit/include/eunit.hrl").
 
 %% Actor Contract Behaviors Callbacks
 
@@ -13,10 +14,17 @@
 -export([create/0]).
 
 create() ->
-   actor_contract:create(?MODULE, actor_product, off, undefined, 10, []).
+   actor_contract:create(?MODULE, 450, undefined, off,0, []).
 
-answer(ProductConfig, _) ->
-		{answer, actor_contract:get_state(ProductConfig), actor_contract:get_data(ProductConfig)}.
+answer(ProductConfig, _ ) ->
+		{answer, actor_contract:get_state(ProductConfig), actor_contract:get_list_data(ProductConfig)}.
 
+%% ===================================================================
+%% Tests
+%% ===================================================================
 
+answer_test() ->	
+	{ok, Prod} = create(),
+	NewProd = actor_contract:add_data(Prod,{21,05,02, q2}),
+	{ answer, off, [{21,05,02, q2}]} = answer(NewProd, undefined).
 
