e4a758bd55c6af3d63c4a4d92e487ba8c448ecd2
Videl Smith <videl@fslhome.org>
2014-06-18 15:51:24 +0200

    Beginning of loops.

--- modelisation/src/actor_rfid.erl
+++ modelisation/src/actor_rfid.erl
@@ -11,9 +11,17 @@
 	answer/2
 	]).
 
+%%
+
+-export([
+	idling/1,
+	powered/1,
+	processing/2,
+	worker_loop/3]).
+
 %% Behavior implementation
 create() ->
-	actor_contract:create(?MODULE, rfid, 0).
+	actor_contract:create(?MODULE, rfid, 1).
 
 answer(RFIDConfig, {actor_product, ProductConfig, id}) ->
 	actor_contract:work(actor_contract:get_work_time(RFIDConfig)),
@@ -28,6 +36,45 @@ answer(RFIDConfig, {actor_product, ProductConfig, id}) ->
 answer(RFIDConfig, Request) ->
 	actor_contract:answer(RFIDConfig, Request).
 
+%% Main loop
+idling(Config) ->
+	receive
+		{start} ->
+			?MODULE:powered(actor_contract:set_state(Config, on));
+		{Sender, actor_product, _, _} ->
+			Sender ! {state, actor_contract:get_state(Config)},
+			?MODULE:idling(Config);
+		_ ->
+			?MODULE:idling(Config)
+	end.
+
+powered(Config) ->
+	receive
+		{stop} ->
+			?MODULE:idling(actor_contract:set_state(Config, off));
+		{Sender, Request} -> 
+			Pid = spawn(?MODULE, worker_loop, [self(), Config, Request]),
+			?MODULE:processing(actor_contract:set_state(Config, processing), Pid);
+		_ ->
+			?MODULE:powered(Config)
+	end.
+
+processing(Config, MainWorker) ->
+	receive
+		{Sender, _Request} ->
+			Sender ! {state, busy};
+			?MODULE:processing(Config, MainWorker);
+		{MainWorker, end_of_work, {NewConfig, LittleAnswer, Destination}} ->
+			% Find destination in 'out' pool
+			% Send LittleAnswer
+			?MODULE:powered(actor_contract:set_state(NewConfig, on));
+		_ ->
+			?MODULE:processing(Config, MainWorker)
+	end.
+
+worker_loop(Master, MasterConfig, Request) ->
+	FullAnswer = ?MODULE:answer(MasterConfig, Request),
+	Master ! {self(), end_of_work, FullAnswer}.
 
 %% Tests
 
