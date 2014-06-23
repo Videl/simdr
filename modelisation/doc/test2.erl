1524a25e5ce58cbaca8e1d8a4b7e87a5697ee06a
Videl Smith <videl@fslhome.org>
2014-06-23 10:43:14 +0200

    Removed useless text.

--- modelisation/doc/test2.erl
+++ modelisation/doc/test2.erl
@@ -1,20 +1,6 @@
- NW = spawn(actor_workstation, idling, [actor_workstation:create()]).
-<0.45.0>
-2> NQ = spawn(actor_basic_queue, idling, [actor_basic_queue:create(NW)]).
-<0.47.0>
-3> NQ ! {start}
-3> .
-{start}
-4> NW ! {start}.
-{start}
-Testing... []
-Nothing to send!!!
-Testing... []      
-Nothing to send!!! 
-Testing... []                    
-Nothing to send!!!               
-Testing... []                    
-Nothing to send!!!               
-Testing... []                                  
-Nothing to send!!!                             
-5> NQ ! {self(), {actor_product, actor_product:create(), register}}.
+NW = spawn(actor_workstation, idling, [actor_workstation:create()]).
+NQ = spawn(actor_basic_queue, idling, [actor_basic_queue:create(NW)]).
+NQ ! {start}.
+NW ! {start}.
+                            
+NQ ! {self(), {actor_product, actor_product:create(), register}}.
