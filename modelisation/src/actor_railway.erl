a6bf52828f4bea3875776c3ab4d50ebb50c605a9
Videl Smith <videl@fslhome.org>
2014-06-26 11:30:47 +0200

    Cleanup.

--- modelisation/src/actor_railway.erl
+++ modelisation/src/actor_railway.erl
@@ -7,13 +7,7 @@
 
 -export([
 	create/0,
-	answer/2,
-	idling/1,
-	processing/2, 
-	logical_work/3,
-	wait/3,
-	physical_work/3,
-	send_rfid/2]).
+	answer/2]).
 
 %% Behavior implementation
 
@@ -73,78 +67,78 @@ answer(RailwayConfig, Request) ->
 	actor_contract:answer(RailwayConfig, Request).
 
 
-idling(Config) ->
- 	actor_contract : idling(Config).
+% idling(Config) ->
+%  	actor_contract : idling(Config).
 
-processing(Config, NbWorker) ->
-	receive
-		{Sender, {actor_product,ProdConf}} ->
-			Request= {actor_product,ProdConf},
-			[N] = actor_contract:get_option(Config, capacity),
-			case NbWorker+1> N of
-				false -> Sender ! { self(), {control, ok, Request}},
-						spawn(?MODULE, send_rfid, [Config, ProdConf]),
-						spawn(?MODULE, physical_work, [self(), Config, Request]),
-						?MODULE:processing(actor_contract:set_state(Config, processing), NbWorker+1);
+% processing(Config, NbWorker) ->
+% 	receive
+% 		{Sender, {actor_product,ProdConf}} ->
+% 			Request= {actor_product,ProdConf},
+% 			[N] = actor_contract:get_option(Config, capacity),
+% 			case NbWorker+1> N of
+% 				false -> Sender ! { self(), {control, ok, Request}},
+% 						spawn(?MODULE, send_rfid, [Config, ProdConf]),
+% 						spawn(?MODULE, physical_work, [self(), Config, Request]),
+% 						?MODULE:processing(actor_contract:set_state(Config, processing), NbWorker+1);
 
-				_-> Sender ! { self(), {control, full,{actor_contract : get_work_time(Config), Request}}},
-						?MODULE:processing(Config, NbWorker)
+% 				_-> Sender ! { self(), {control, full,{actor_contract : get_work_time(Config), Request}}},
+% 						?MODULE:processing(Config, NbWorker)
 				
-			end;
-		{Sender, {control, full, {Wait_time, Request}}} ->
-			spawn(?MODULE, wait, [self(), Wait_time,{Request, Sender}]),
-			?MODULE:processing(Config, NbWorker);
+% 			end;
+% 		{Sender, {control, full, {Wait_time, Request}}} ->
+% 			spawn(?MODULE, wait, [self(), Wait_time,{Request, Sender}]),
+% 			?MODULE:processing(Config, NbWorker);
 
-		{_Sender, Request} ->
-			spawn(?MODULE, logical_work, [self(), Config, Request]),
-			?MODULE:processing(Config, NbWorker);
+% 		{_Sender, Request} ->
+% 			spawn(?MODULE, logical_work, [self(), Config, Request]),
+% 			?MODULE:processing(Config, NbWorker);
 
-		{_Worker, end_physical_work, {NewConfig, LittleAnswer, Destination}} ->
-			% Find destination in 'out' pool
-			% Send LittleAnswer
-		 	{actor_product, ConfProd, _} = LittleAnswer,
-			send_message({{actor_product, ConfProd}, Destination}),
-			?MODULE:processing(actor_contract:set_state(NewConfig, free), NbWorker-1);
+% 		{_Worker, end_physical_work, {NewConfig, LittleAnswer, Destination}} ->
+% 			% Find destination in 'out' pool
+% 			% Send LittleAnswer
+% 		 	{actor_product, ConfProd, _} = LittleAnswer,
+% 			send_message({{actor_product, ConfProd}, Destination}),
+% 			?MODULE:processing(actor_contract:set_state(NewConfig, free), NbWorker-1);
 	
-		{_Worker, end_logical_work, {NewConfig, LittleAnswer, Destination}} ->
-			send_message({LittleAnswer, Destination}),
-			%io:format(" Nouvelle config ~w ~n",[NewConfig]),
-			?MODULE:processing(NewConfig, NbWorker);
-		_ ->
-			?MODULE:processing(Config, NbWorker)
-	end.
+% 		{_Worker, end_logical_work, {NewConfig, LittleAnswer, Destination}} ->
+% 			send_message({LittleAnswer, Destination}),
+% 			%io:format(" Nouvelle config ~w ~n",[NewConfig]),
+% 			?MODULE:processing(NewConfig, NbWorker);
+% 		_ ->
+% 			?MODULE:processing(Config, NbWorker)
+% 	end.
 
-send_rfid( Conf, ProdConf) ->
-	case actor_contract:get_option(Conf, rfid) of 
-		[RFID] -> RFID ! {self(), {actor_product, ProdConf}};
-		_ -> {nothing}
-	end.
+% send_rfid( Conf, ProdConf) ->
+% 	case actor_contract:get_option(Conf, rfid) of 
+% 		[RFID] -> RFID ! {self(), {actor_product, ProdConf}};
+% 		_ -> {nothing}
+% 	end.
 
-send_message( {Ans, [Dest]}) when is_pid(Dest) -> 
-io:format("Conveyor Sending: ~w to ~w.~n", [ Ans, Dest]),
-	Dest ! {self(), Ans};
-send_message( {Ans, Dest}) when is_pid(Dest) -> 
-	Dest ! {self(), {Ans}};
-send_message({Ans, Dest}) ->
-	%% @TODO: decider de la destination
-	io:format("Conveyor Sending: ~w to ~w.~n", [Ans, Dest]).
+% send_message( {Ans, [Dest]}) when is_pid(Dest) -> 
+% io:format("Conveyor Sending: ~w to ~w.~n", [ Ans, Dest]),
+% 	Dest ! {self(), Ans};
+% send_message( {Ans, Dest}) when is_pid(Dest) -> 
+% 	Dest ! {self(), {Ans}};
+% send_message({Ans, Dest}) ->
+% 	%% @TODO: decider de la destination
+% 	io:format("Conveyor Sending: ~w to ~w.~n", [Ans, Dest]).
 
-wait(Pid ,Wait_time, {Ans, Dest}) when is_pid(Dest)->
-	actor_contract:work(Wait_time),
-	Dest ! {Pid, {Ans}};
+% wait(Pid ,Wait_time, {Ans, Dest}) when is_pid(Dest)->
+% 	actor_contract:work(Wait_time),
+% 	Dest ! {Pid, {Ans}};
 
-wait(_Pid ,Wait_time, {Ans, Dest}) when is_pid(Dest)->
-	actor_contract:work(Wait_time),
-	io:format("Conveyor Sending: ~w to ~w.~n", [Ans, Dest]).
+% wait(_Pid ,Wait_time, {Ans, Dest}) when is_pid(Dest)->
+% 	actor_contract:work(Wait_time),
+% 	io:format("Conveyor Sending: ~w to ~w.~n", [Ans, Dest]).
 
-physical_work(Master, MasterConfig, Request) ->
-	io:format("Conveyor work ~w.~n", [{MasterConfig,Request}]),
-	FullAnswer = ?MODULE:answer(MasterConfig, Request),
-	Master ! {self(), end_physical_work, FullAnswer}.
+% physical_work(Master, MasterConfig, Request) ->
+% 	io:format("Conveyor work ~w.~n", [{MasterConfig,Request}]),
+% 	FullAnswer = ?MODULE:answer(MasterConfig, Request),
+% 	Master ! {self(), end_physical_work, FullAnswer}.
 
-logical_work(Master, MasterConfig, Request) ->
-	FullAnswer = ?MODULE:answer(MasterConfig, Request),
-	Master ! {self(), end_logical_work, FullAnswer}.
+% logical_work(Master, MasterConfig, Request) ->
+% 	FullAnswer = ?MODULE:answer(MasterConfig, Request),
+% 	Master ! {self(), end_logical_work, FullAnswer}.
 
 
 %Internal API
