ce5c255d730582a63483f8a12a8aba69b8e03303
Videl Smith <videl@fslhome.org>
2014-07-08 16:11:13 +0200

    Fix: tests.

--- modelisation/src/actor_workstation.erl
+++ modelisation/src/actor_workstation.erl
@@ -85,11 +85,11 @@ data_filler_test_() ->
 	[
 		%%% Test: last data exists in product
 		?_assertMatch(
-			{_ErlangNow, _Time, {quality,became,Quality,because,'of',{BaseWS}}},
+			{_ErlangNow, _Time, _BasePO, {quality,became,Quality,because,'of',{BaseWS}}},
 			LastDataPO),
 		%%% Test: last data exists in workstation
 		?_assertMatch(
-			{_ErlangNow, _Time, {changed,quality,'of',product, {BasePO, for, Quality}}}, 
+			{_ErlangNow, _Time, BaseWS, {changed,quality,'of',product, {BasePO, for, Quality}}}, 
 			LastDataWS)
 	].
 
