6235bf83ca96b174b42edac4c817111c0187bc22
MarionLy <marion.ly@telecomnancy.net>
2014-07-09 15:16:57 +0200

    test ok

--- modelisation/src/actor_workstation_assembly.erl
+++ modelisation/src/actor_workstation_assembly.erl
@@ -26,7 +26,8 @@ create() ->
 	%%% module, state, work_time
 	Ac1 = actor_contract:create(?MODULE, off, 10),
 	Ac2 = actor_contract:set_capacity(Ac1, 1),
-	Ac2.
+	Ac3 = actor_contract:add_option(Ac2, order, {1,0,1,0}),
+	Ac3.
 
 create({Stop, Manip, Evac}) ->
 	Ac1 = create(),
@@ -34,7 +35,8 @@ create({Stop, Manip, Evac}) ->
 	actor_contract:set_option(Ac1, manipulation, Manip),
 	actor_contract:set_option(Ac1, evacuation, Evac),
 	Ac2 =actor_contract:set_work_time(Stop+Manip+Evac),
-	Ac2.
+	Ac3 = actor_contract:add_option(Ac2, order, {1,0,1,0}),
+	Ac3.
 
 answer(WSConfig, {actor_product, ProductConfig}) ->
 	actor_contract:work(actor_contract:get_work_time(WSConfig)),
@@ -44,7 +46,7 @@ answer(WSConfig, {actor_product, ProductConfig}) ->
 	%%% List data fillers
 	{NewWSConfig, NewProductConfigBis} = actor_contract:add_to_list_data(
 		WSConfig, {changed,assembly,'of',product, {ProductConfig, for, Transfo}}, 
-		NewProductConfig, {quality,became,Transfo,because,'of',{WSConfig}}),
+		NewProductConfig, {assembly,became,Transfo,because,'of',{WSConfig}}),
 	%%% Answer
 	{NewWSConfig, 
 	{actor_product, NewProductConfigBis, Transfo}, 
@@ -60,32 +62,32 @@ answer(WSConfig, Request) ->
 -ifdef(TEST).
 
 answer_test_() ->
-	ActorWS = actor_contract:set_work_time(actor_workstation:create(),1),
+	ActorWS = actor_contract:set_work_time(actor_workstation_assembly:create(),1),
 	ActorProductOne = actor_product:create(),
 	{_, {actor_product, ActorProductTwo, _Quality}, _Destination} = 
-		actor_workstation:answer(ActorWS, {actor_product, ActorProductOne}),
+		actor_workstation_assembly:answer(ActorWS, {actor_product, ActorProductOne}),
 	[
 		%%% Test: quality of a product is different
 		?_assert(
-			raw =/= actor_contract:get_state(ActorProductTwo)
+			processed =/= actor_contract:get_state(ActorProductTwo)
 		)
 	].
 
 data_filler_test_() ->
 	BaseWS = create(),
 	BasePO = actor_product:create(),
-	{NewWS, {actor_product, NewPO, Quality}, _} = 
+	{NewWS, {actor_product, NewPO, Transfo}, _} = 
 	 	answer(BaseWS, {actor_product, BasePO}),
 	LastDataWS = actor_contract:get_data(NewWS),
 	LastDataPO = actor_contract:get_data(NewPO),
 	[
 		%%% Test: last data exists in product
 		?_assertMatch(
-			{_ErlangNow, _Time, {quality,became,Quality,because,'of',{BaseWS}}},
+			{_ErlangNow, _Time, {assembly,became,Transfo,because,'of',{BaseWS}}},
 			LastDataPO),
 		%%% Test: last data exists in workstation
 		?_assertMatch(
-			{_ErlangNow, _Time, {changed,quality,'of',product, {BasePO, for, Quality}}}, 
+			{_ErlangNow, _Time, {changed,assembly,'of',product, {BasePO, for, Transfo}}}, 
 			LastDataWS)
 	].
 
