df8f0402fcc26bf50760cf244be397a91246b99d
Videl Smith <videl@fslhome.org>
2014-07-17 16:57:54 +0200

    Refactor: actor_contract more general.

--- modelisation/src/actor_scanner.erl
+++ modelisation/src/actor_scanner.erl
@@ -1,9 +1,9 @@
 -module(actor_scanner).
 -include("app_configuration.hrl").
 
-% -ifdef(TEST).
-% -include_lib("eunit/include/eunit.hrl").
-% -endif.
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
 
 -behaviour(actor_contract).
 
@@ -28,7 +28,7 @@ answer(ScannerConfig, {actor_product, ProductConfig}) ->
 	true -> 
 		Transfo = actor_contract:get_option(ProductConfig, processed),
 		[{Quality, Luck}] = Transfo,
-		FinalQuality = actor_contract:random_weighted(Luck, Quality),
+		FinalQuality = random_weighted(Luck, Quality),
 		ProductConf = actor_contract:set_option(
 			ProductConfig, 
 			quality, 
@@ -47,3 +47,121 @@ answer(ScannerConfig, {actor_product, ProductConfig}) ->
 
 answer(RFIDConfig, Request) ->
 	actor_contract:answer(RFIDConfig, Request).
+
+
+%% ===================================================================
+%% Internal API
+%% ===================================================================
+
+random_weighted(Luck, Objective) ->
+    Rnd = random:uniform(100),
+    %%% 0 can't arise in Rnd, so we go up to 100
+    Result = case Luck >= Rnd of
+				true ->
+					Objective;
+				 _ ->
+				    random(Objective)
+	     	 end,
+    Result.	
+
+random('Q1') ->
+    Result = case random:uniform(2) of
+		 1 -> % Medium quality
+		     'Q2';
+		 2 -> % Bad quality
+		     'Q3'
+	     end,
+    Result;
+random('Q2') ->
+    Result = case random:uniform(2) of
+		 1 -> % Good quality
+		     'Q1';
+		 2 -> % Bad quality
+		     'Q3'
+	     end,
+    Result;
+random('Q3') ->
+    Result = case random:uniform(2) of
+		 1 -> % Good quality
+		     'Q1';
+		 2 -> % Medium quality
+		     'Q2'
+	     end,
+    Result.
+
+%% ===================================================================
+%% Tests
+%% ===================================================================
+
+-ifdef(TEST).
+
+random_list_maker(List, _, 0) ->
+    List;
+random_list_maker(List, {Objective, Luck}, X) ->
+    NewList = [random_weighted(Luck, Objective)] ++ List,
+    random_list_maker(NewList, {Objective, Luck}, X-1).
+
+random_list_maker({Objective, Luck}, X) ->
+	random_list_maker([], {Objective, Luck}, X).
+
+%%% List: list to traverse
+%%% Chr: character to count
+%%% Returns the number of times the character has been found.
+%%% @end
+occurences_in_a_list(List, Chr) ->
+    Fonc = fun(X, N) when X =:= Chr ->
+		   N+1;
+	      (_, N) -> N
+	   end,
+    lists:foldl(Fonc, 0, List).
+
+percent(Max, No) ->
+    A = (No/Max)*100,
+    B = float_to_list(A, [{decimals, 0}]),
+    {C, _} = string:to_integer(B),
+    C.
+
+random_test_() ->
+	random:seed(erlang:now()),
+	ListQ1_100 = random_list_maker({'Q1', 100}, 10000),
+	ListQ1_20  = random_list_maker({'Q1', 20}, 10000),
+	ListQ1_50  = random_list_maker({'Q1', 50}, 10000),
+	ListQ3_80  = random_list_maker({'Q3', 80}, 10000),
+	ListQ3_30  = random_list_maker({'Q3', 30}, 10000),
+	ListQ3_60  = random_list_maker({'Q3', 60}, 10000),
+	ListQ2_85  = random_list_maker({'Q2', 85}, 10000),
+	ListQ2_25  = random_list_maker({'Q2', 25}, 10000),
+	ListQ2_55  = random_list_maker({'Q2', 55}, 10000),
+	%%% Answer
+	ResultQ1_100 = percent(10000, occurences_in_a_list(ListQ1_100, 'Q1')),
+	ResultQ1_20  = percent(10000, occurences_in_a_list(ListQ1_20, 'Q1')),
+	ResultQ1_50  = percent(10000, occurences_in_a_list(ListQ1_50, 'Q1')),
+	ResultQ3_80  = percent(10000, occurences_in_a_list(ListQ3_80, 'Q3')),
+	ResultQ3_30  = percent(10000, occurences_in_a_list(ListQ3_30, 'Q3')),
+	ResultQ3_60  = percent(10000, occurences_in_a_list(ListQ3_60, 'Q3')),
+	ResultQ2_85  = percent(10000, occurences_in_a_list(ListQ2_85, 'Q2')),
+	ResultQ2_25  = percent(10000, occurences_in_a_list(ListQ2_25, 'Q2')),
+	ResultQ2_55  = percent(10000, occurences_in_a_list(ListQ2_55, 'Q2')),
+	MaxError = 3,
+	[
+		?_assert(ResultQ1_100 =:= 100),
+		?_assert(ResultQ1_20 < (20 + MaxError)),
+		?_assert(ResultQ1_20 > (20 - MaxError)),
+		?_assert(ResultQ1_50 < (50 + MaxError)),
+		?_assert(ResultQ1_50 > (50 - MaxError)),
+		?_assert(ResultQ3_80 < (80 + MaxError)),
+		?_assert(ResultQ3_80 > (80 - MaxError)),
+		?_assert(ResultQ3_30 < (30 + MaxError)),
+		?_assert(ResultQ3_30 > (30 - MaxError)),
+		?_assert(ResultQ3_60 < (60 + MaxError)),
+		?_assert(ResultQ3_60 > (60 - MaxError)),
+		?_assert(ResultQ2_85 < (85 + MaxError)),
+		?_assert(ResultQ2_85 > (85 - MaxError)),
+		?_assert(ResultQ2_25 < (25 + MaxError)),
+		?_assert(ResultQ2_25 > (25 - MaxError)),
+		?_assert(ResultQ2_55 < (55 + MaxError)),
+		?_assert(ResultQ2_55 > (55 - MaxError))
+
+	].
+
+-endif.
