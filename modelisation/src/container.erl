b9f1e28c6fc6dc1d6b437ffbb9383ddc02655cf7
Videl Smith <videl@fslhome.org>
2014-07-17 11:41:00 +0200

    Add: supervisor_end to terminate products when end of path.

--- modelisation/src/container.erl
+++ modelisation/src/container.erl
@@ -100,7 +100,7 @@ end_of_physical_work(
 					 {Config, NbWorkers}, 
 					 {NewConfig, LittleAnswer, Destination}) ->
 	[TablePid] = actor_contract:get_option(Config, ets), 
-	{actor_product, ProductConfig, Detail} = LittleAnswer,
+	{actor_product, ProductConfig, _Detail} = LittleAnswer,
 	Awaiting = ets:match_object(TablePid, {awaiting, '$1'}),
 	?DLOG(
 		actor_contract:get_module(Config), 
