8645d01b04c36719d6bc92b58ef4cffb8a5d2a60
MarionLy <marion.ly@telecomnancy.net>
2014-07-18 15:53:49 +0200

    ajout suffixe BQ

--- modelisation/src/actor_basic_queue.erl
+++ modelisation/src/actor_basic_queue.erl
@@ -17,25 +17,25 @@
 %% Behavior implementation
 
 create() ->
-	create(actor_contract:random_id()).
+	create(simdr_actor_contract:random_id()).
 
 create(Name ) ->
-	actor_contract:create(?MODULE, Name, [], off, 1, []).
+	simdr_actor_contract:create(?MODULE, Name, [], off, 1, []).
 
 
 %% Possible answer: a new product arriving
 answer(BasicQueueConfig, {actor_product, ProductConfig}) ->
 	%% Work time here means 'deplacement time' of the product, when the queue
 	%% is used as a conveyor
-	actor_contract:work(actor_contract:get_work_time(BasicQueueConfig)),
-	{NewBasicQueueConfig, NewProductConfig} = actor_contract:add_to_list_data(
+	simdr_actor_contract:work(simdr_actor_contract:get_work_time(BasicQueueConfig)),
+	{NewBasicQueueConfig, NewProductConfig} = simdr_actor_contract:add_to_list_data(
 		BasicQueueConfig, {{product,entered,queue},{ProductConfig}}, 
 		ProductConfig, {{entered,queue},{BasicQueueConfig}}),
-	Destination = actor_contract:get_out(BasicQueueConfig),
+	Destination = simdr_actor_contract:get_out(BasicQueueConfig),
 	% Empty to notify the container there is nothing to send, 
 	% not even to supervisor.
 	{NewBasicQueueConfig, {actor_product, NewProductConfig, buffered}, Destination};
 answer(BasicQueueConfig, Request) ->
-	actor_contract:answer(BasicQueueConfig, Request).
+	simdr_actor_default:answer(BasicQueueConfig, Request).
 
 %% Tests
